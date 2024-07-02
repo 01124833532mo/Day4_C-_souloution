1bb7706fe06aa7d93f940054cfaacc5d0b719492
01124833532mo <mohammedhamdi726@gmail.com>
2024-07-02 04:39:25 +0300

    Write a c# Function that accept 4 parameters from user and return result of summation and subtracting of two numbers

--- Assement/Program.cs
+++ Assement/Program.cs
@@ -21,6 +21,14 @@ namespace Assement
         #endregion
 
 
+        #region question 3
+        //public static void Calculate(double a, double b, double c, double d,out double sum,out double sub)
+        //{
+        //    sum = a + b + c + d;
+        //    sub = (a + b) - (c + d);
+        //}
+
+        #endregion
 
 
         static void Main(string[] args)
@@ -44,6 +52,35 @@ namespace Assement
 
             #endregion
 
+            #region Write a c# Function that accept 4 parameters from user and return result of summation and subtracting of two numbers
+            // Console.Write("Enter the first number");
+            // double num1;
+            //bool flag1= double.TryParse(Console.ReadLine(), out num1);
+
+            // Console.Write("Enter the second number: ");
+            // double num2;
+            // bool flag2=   double.TryParse(Console.ReadLine(), out num2);
+
+            // Console.Write("Enter the third number: ");
+            // double num3;
+            // bool flag3 =double.TryParse(Console.ReadLine(), out num3);
+
+            // Console.Write("Enter the fourth number: ");
+            // double num4;
+            // bool flag4= double.TryParse(Console.ReadLine(), out num4);
+
+            // double sumResult;
+            // double subtractResult;
+            // if (!(flag1 && flag2 && flag3 && flag4))
+            // {
+            //     Console.WriteLine("can not ");
+            // }
+            // else {
+            //     Calculate(num1, num2, num3, num4, out sumResult, out subtractResult);
+
+            //     Console.WriteLine($"Summation Result: {sumResult}");
+            //     Console.WriteLine($"Subtraction Result: {subtractResult}"); }
+            #endregion
 
         }
     }
